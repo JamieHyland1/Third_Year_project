608ec593237dd52bec3ae5a0836f9430b753be01
Jamie Hyland <Jamiehyland01@gmail.com>
2016-12-24 13:57:00 +0000

    added database implementation and reconverged web server and websocket server

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var ws = require("ws").Server; //require websocket library
 var express = require('express') // require express library
 var app = express() // initialise express
-var noise = require("simplex-noise") //require noise algorithim
+var MongoClient = require('mongodb').MongoClient
+ , assert = require('assert');
+const perlin = require('pf-perlin');
+ 
+// Create a 3D Perlin Noise generator. 
+const Perlin3D = perlin({dimensions: 1});
+ 
+// Use it to make a 100x100x100 grid of values 
+
+      
 var wss = new ws({
 	port: 3000
 }); //set the WebSocket Server to port 3000
@@ -11,35 +20,49 @@ var wss = new ws({
 
 
 
-wss.on("connection", function (ws) {
-	//setInterval(function (){console.log(genNumber(500))}, 500);
-	setInterval(function () {
-			var x = Math.round(Math.random() * 100) + 1;
-			var data = JSON.stringify(x);
-			ws.send(data)
+wss.on("connection", function (ws)
+{
+	var res = 900;
+	setInterval(function()
+	{
+		var x = Math.floor(Math.random() * 8);
+		var data = Perlin3D.get((x/res))*1000;
+		var msg = JSON.stringify(data);
+		ws.send(msg)
 
-		}, 1000)
-
+	
+	
+	},1000)
+	
 
 	wss.on('close', function () {
 		console.log('Client disconnected.') //log message to console when connection is closed
 	});
 
-	
-		
-
-	
-	ws.onmessage = function(err,event){
-		if(err)
+	ws.onmessage = function(event)
+	{
+			console.log(event.data)
+			var insert = JSON.parse(event.data)
+			var url = 'mongodb://localhost:27017/stockData';
+			// Use connect method to connect to the Server
+			MongoClient.connect(url, function(err, db)
 			{
-				throw err;
-			}
-		else{
-					console.log(event.data)
+				assert.equal(null, err);
+				console.log("Connected correctly to server");
 
+				// Insert a single document
+				db.collection('stockData').insertOne(insert, function(err, r)
+				{
+				 assert.equal(null, err);
+				 assert.equal(1, r.insertedCount);
+
+				 db.close();
+				});
+			});
 		}
-	}
-})
+	
+
+});
 
 console.log("WebSocket Server running on port 3000")
 console.log("Web Server running on port 8081")
