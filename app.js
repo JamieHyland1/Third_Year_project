25579bb3000294473e437f9c73e30fd5d9216972
Jamie Hyland <Jamiehyland01@gmail.com>
2017-01-08 13:07:40 +0000

    added retrieval query to database

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var ws = require("ws").Server; //require websocket library
+var html = require('html')
 var express = require('express') // require express library
 var app = express() // initialise express
 var MongoClient = require('mongodb').MongoClient
@@ -13,6 +14,9 @@ var wss = new ws({
 	port: 3000
 }); //set the WebSocket Server to port 3000
 
+	var url = 'mongodb://localhost:27017/stockData';	
+
+
 //=============================================================================================================================================
 
 
@@ -20,46 +24,53 @@ var wss = new ws({
 
 wss.on("connection", function  (ws)
 {
-		
 	var beginning = 250
 	var t = 0.000000000000001;
 	var insertArray = []; // will hold every value generated every second for one minute 
-	var count = 0; // counter for when to insert data to the database
-	setInterval(function()
+
+//	MongoClient.connect(url, function (err, db)
+//	{
+//		assert.equal(null, err);
+//		getData(db, function () 
+//		{
+//			db.close();
+//		});
+//	});
+	
+	function generateData()
 	{
-		var x = Math.random().toFixed(2);
+		var x = Math.floor(Math.random()*100);
 		var data = beginning + Perlin.noise(t);
-		insertArray.push(data)
+		insertArray.push(x)
 		var msg = JSON.stringify(data);
-		ws.send(msg, function(error){
+		ws.send(msg, function(error)
+		{
 			//Prevents the server from crashing if connection drops
-			console.log("connection dropped")
 		})
 
-	t++
-	count++;
-	if(count == 60)
-	{
+	console.log(insertArray.length)
+	if(insertArray.length == 60) //The length of the array will act as a counter to when the data should be inserted i.e if the length of the
+	{//								array is 60, 60 seconds will have passed.	
+		
 		insertData(insertArray) //calling function to insert data to mongo on a per minute basis
-		insertArray = []; // wipe the array of all current index's
-		counter = 0; // set counter to 0 to begin the loop again
+		insertArray = []; // wipe the array of all current index's this will reset the counter for the next minute
 	}
-	
+		
+	setTimeout(function()
+	{
+		generateData()
+		
 	},1000)
+	
+	}
 	
 
 	ws.onclose = function(event)
 	{
-		console.log("Connection Closed")
-	
+	 console.log("Connection Closed")
 	}
-	ws.onmessage = function(event)
-	{
-			console.log(event.data)
-			var insert = JSON.parse(event.data)
-		
-		}
 	
+	//generateData();
 	
 });
 
@@ -78,7 +89,7 @@ function insertData(array)
 	var low = array[0];
 	// console.log(high)
 	var obj = {"high": high, "low": low, "open": open, "close": close};
-	
+	console.log(obj)
 	var url = 'mongodb://localhost:27017/stockData'; 
 	// Use connect method to connect to the Server
 	MongoClient.connect(url, function(err, db)
@@ -92,9 +103,22 @@ function insertData(array)
 			assert.equal(1, r.insertedCount);
 			
 			db.close();
+			console.log("database closed")
 		});
 	});
 }
+
+var getData = function(db, callback) {
+   var cursor =db.collection('stockData').find();
+   cursor.each(function(err, doc) {
+      assert.equal(err, null);
+      if (doc != null) {
+       //  console.log(doc);
+      } else {
+         callback();
+      }
+   });
+};
 
 console.log("WebSocket Server running on port 3000")
 console.log("Web Server running on port 8081")
@@ -103,6 +127,21 @@ app.use(express.static('public')); //set up express to send static files
 app.get('/index.html', function (req, res) {
 	res.sendFile(__dirname + "/" + "index.html");
 })
+
+app.get('/getData', function(req, res, next) {
+  var resultArray = [];
+  MongoClient.connect(url, function(err, db){
+	  assert.equal(null,err)
+	  var cursor = db.collection("stockData").find();
+	  cursor.forEach(function(doc,err){
+		assert.equal(null,err)
+		resultArray.push(doc)
+	 }, function(){
+		  db.close();
+		  res.render('/index.html', {local_variables: resultArray})
+	  })
+  })
+});
 
 var server = app.listen(8081, function () {
 	var host = server.address().address
