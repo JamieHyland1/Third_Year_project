539dd6468d3424a38e9f7d2f95146b09ccc4db9a
Jamie Hyland <Jamiehyland01@gmail.com>
2017-01-08 15:04:08 +0000

    Streamlined database query for retrieving data

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 var ws = require("ws").Server; //require websocket library
-var html = require('html')
 var express = require('express') // require express library
 var app = express() // initialise express
-var MongoClient = require('mongodb').MongoClient
- , assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 var PerlinGenerator = require("proc-noise");
 var Perlin = new PerlinGenerator(); // seeds itself if no seed is given as an argument 
 
-// Use it to make a 100x100x100 grid of values 
 
       
 var wss = new ws({
 	port: 3000
 }); //set the WebSocket Server to port 3000
 
-	var url = 'mongodb://localhost:27017/stockData';	
+	var url = 'mongodb://127.0.0.1:27017/stockData'; 	
 
 
 //=============================================================================================================================================
@@ -27,28 +25,33 @@ wss.on("connection", function  (ws)
 	var beginning = 250
 	var t = 0.000000000000001;
 	var insertArray = []; // will hold every value generated every second for one minute 
-
-//	MongoClient.connect(url, function (err, db)
-//	{
-//		assert.equal(null, err);
-//		getData(db, function () 
-//		{
-//			db.close();
-//		});
-//	});
 	
+	MongoClient.connect(url, function(err,db){ //set up connection to mongodb takes two parameters a url for the db and a callback function 
+
+		assert.equal(err,null) //check to see if there any errors connecting to the database
+		var cursor = db.collection('stockData').find(); // cursor will be an array of objects retrieved from the database
+		cursor.forEach(function(doc, err){ //loop through the cursor
+			assert.equal(null,err) //check for errors
+			//retrieveArray.push(doc); //push each document the cursor points toward to the retrieveArray
+			ws.send(JSON.stringify(doc), function(error){})
+			
+		}, function(){ 
+			db.close(); //close the database once the query is finished
+			generateData(); //call the generate data function to start sending values to client
+		});
+	});
 	function generateData()
 	{
 		var x = Math.floor(Math.random()*100);
 		var data = beginning + Perlin.noise(t);
 		insertArray.push(x)
-		var msg = JSON.stringify(data);
+		var msg = JSON.stringify(x);
 		ws.send(msg, function(error)
 		{
 			//Prevents the server from crashing if connection drops
 		})
 
-	console.log(insertArray.length)
+	//console.log(insertArray.length)
 	if(insertArray.length == 60) //The length of the array will act as a counter to when the data should be inserted i.e if the length of the
 	{//								array is 60, 60 seconds will have passed.	
 		
@@ -89,7 +92,7 @@ function insertData(array)
 	var low = array[0];
 	// console.log(high)
 	var obj = {"high": high, "low": low, "open": open, "close": close};
-	console.log(obj)
+	//console.log(obj)
 	var url = 'mongodb://localhost:27017/stockData'; 
 	// Use connect method to connect to the Server
 	MongoClient.connect(url, function(err, db)
@@ -108,17 +111,7 @@ function insertData(array)
 	});
 }
 
-var getData = function(db, callback) {
-   var cursor =db.collection('stockData').find();
-   cursor.each(function(err, doc) {
-      assert.equal(err, null);
-      if (doc != null) {
-       //  console.log(doc);
-      } else {
-         callback();
-      }
-   });
-};
+
 
 console.log("WebSocket Server running on port 3000")
 console.log("Web Server running on port 8081")
@@ -128,20 +121,7 @@ app.get('/index.html', function (req, res) {
 	res.sendFile(__dirname + "/" + "index.html");
 })
 
-app.get('/getData', function(req, res, next) {
-  var resultArray = [];
-  MongoClient.connect(url, function(err, db){
-	  assert.equal(null,err)
-	  var cursor = db.collection("stockData").find();
-	  cursor.forEach(function(doc,err){
-		assert.equal(null,err)
-		resultArray.push(doc)
-	 }, function(){
-		  db.close();
-		  res.render('/index.html', {local_variables: resultArray})
-	  })
-  })
-});
+
 
 var server = app.listen(8081, function () {
 	var host = server.address().address
